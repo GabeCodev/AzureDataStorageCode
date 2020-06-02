796b31e37f19f6d5cf64b111a16e6188c4026041
Gabriel T. Trenzado <gabecodev@gmail.com>
2020-06-02 16:40:08 -0600

    Created C# and XML files

---
+++ app.cs
@@ -0,0 +1,8 @@
+//example, to retrieve a list of blobs in C#, we could use the following code snippet
+CloudBlobDirectory directory = ...;
+foreach (IEnumerable<IListBlobItem> blob in directory.ListBlobs(
+                useFlatBlobListing: true,
+                blobListingDetails: BlobListingDetails.All))
+{
+    // Work with blob item .. could be page blob, block blob, etc.
+}
